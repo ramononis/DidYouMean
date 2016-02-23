e55325a3fff085743da7351d9215f82115c14d86
Magnificus36 <magnificus@outlook.com>
2016-02-23 16:22:36 +0100

    Code deduplication

--- src/autoComplete/tree/Root.java
+++ src/autoComplete/tree/Root.java
@@ -1,29 +1,11 @@
 package autoComplete.tree;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * Created by Frans on 2016-02-23.
  */
 public class Root extends Node {
-    private final Set<Element> children;
-
     public Root() {
         super((char)-1, 0);
-        children = new HashSet<>();
-    }
-
-    public Set<Element> getChildren() {
-        return new HashSet<>(children);
-    }
-
-    public boolean addChild(Node node) {
-        return children.add(node);
-    }
-
-    public boolean removeChild(Node node) {
-        return children.remove(node);
     }
 
     @Override
