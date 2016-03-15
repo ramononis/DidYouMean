a257eee95891d430b997c963c0b1dabe00c62f89
Tim108 <tsonderen108@gmail.com>
2016-03-15 11:10:19 +0100

    tests

---
+++ test/autoComplete/AutoCompleterTest.java
@@ -0,0 +1,19 @@
+package autoComplete;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Tim on 15-3-2016.
+ */
+public class AutoCompleterTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetTopN() throws Exception {
+
+    }
+}
---
+++ test/database/CSVControlTest.java
@@ -0,0 +1,22 @@
+package database;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Tim on 15-3-2016.
+ */
+public class CSVControlTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetData() throws Exception {
+
+    }
+}
