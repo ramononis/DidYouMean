bd9e4a72eb041ce89bd473b1717f1e2ead658553
Tim108 <tsonderen108@gmail.com>
2016-03-15 16:28:40 +0100

    tests

--- test/database/CSVControlTest.java
+++ test/database/CSVControlTest.java
@@ -15,19 +15,21 @@ import static org.junit.Assert.assertThat;
  */
 public class CSVControlTest {
     private CSVControl c;
+    private CSVControl deadc;
 
     @Before
     public void setUp() throws Exception {
         c = new CSVControl(new String[]{"./csv/DataTest.csv"});
+        deadc = new CSVControl(new String[]{"je moeder"});
     }
 
     @Test
     public void testGetData() throws Exception {
+        deadc.getData(); // should print file not found.
+
         HashMap<String,Integer> testData = c.getData();
 
-        System.out.println(testData.toString());
-
-        assertThat(testData.size(), is(7)); // skips everything it shouldn't/can't parse
+        assertThat(testData.size(), is(8)); // skips everything it shouldn't/can't parse
 
         assertThat(testData.get("1"), is(190));
         assertThat(testData.get("one"), is(0));
@@ -36,5 +38,6 @@ public class CSVControlTest {
         assertThat(testData.get("four"), is(128634356));
         assertThat(testData.get("five,six"), is(225337093));
         assertThat(testData.get("inch\""), is(229600));
+        assertThat(testData.get("double"), is(400));
     }
 }
