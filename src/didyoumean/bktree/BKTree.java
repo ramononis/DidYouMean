b8cec0a6a1174e3c317b6bdf638fc4413828a836
swordiemen <tim.blokk@gmail.com>
2016-03-15 16:40:30 +0100

    Diagram added, LD calculation method added in BKTree.

--- src/didyoumean/bktree/BKTree.java
+++ src/didyoumean/bktree/BKTree.java
@@ -109,6 +109,29 @@ public class BKTree {
         return bestNode == null ? "" : bestNode.getName();
     }
 
+    public static int calculateLD(String word1, String word2){
+        //based on http://rosettacode.org/wiki/Levenshtein_distance#Java
+        word1 = word1.toLowerCase();
+        word2 = word2.toLowerCase();
+
+        int[] costs = new int[word2.length() + 1];
+        for (int j = 0; j < costs.length; j++) {
+            costs[j] = j;
+        }
+        for (int i = 1; i <= word1.length(); i++) {
+            // j == 0; nw = lev(i - 1, j)
+            costs[0] = i;
+            int nw = i - 1;
+            for (int j = 1; j <= word2.length(); j++) {
+                int costj = Math.min(1 + Math.min(costs[j], costs[j - 1]),
+                        word1.charAt(i - 1) == word2.charAt(j - 1) ? nw : nw + 1);
+                nw = costs[j];
+                costs[j] = costj;
+            }
+        }
+        return costs[word2.length()];
+    }
+
     public static Node getRoot(){
         return root;
     }
