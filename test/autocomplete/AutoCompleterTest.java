8b47fce1ec8f6d8bd41af0fa8c8677c685b6bace
Tim108 <tsonderen108@gmail.com>
2016-03-15 17:02:47 +0100

    tests

--- test/autocomplete/AutoCompleterTest.java
+++ test/autocomplete/AutoCompleterTest.java
@@ -1,4 +1,7 @@
-package autoComplete;
+package autocomplete;
+
+import autocomplete.AutoCompleter;
+import database.CSVControl;
 
 import static org.junit.Assert.*;
 
@@ -6,10 +9,17 @@ import static org.junit.Assert.*;
  * Created by Tim on 15-3-2016.
  */
 public class AutoCompleterTest {
+    AutoCompleter AC0;
+    AutoCompleter AC1;
+    AutoCompleter AC2;
+    AutoCompleter AC3;
 
     @org.junit.Before
     public void setUp() throws Exception {
-
+        AC0 = new AutoCompleter(new CSVControl(new String[]{"wrong path"}));
+        AC1 = new AutoCompleter(new CSVControl(new String[]{"./csv/DataTest1.csv"}));
+        AC2 = new AutoCompleter(new CSVControl(new String[]{"./csv/DataTest2.csv"}));
+        AC3 = new AutoCompleter(new CSVControl(new String[]{"./csv/DataTest3.csv"}));
     }
 
     @org.junit.Test
