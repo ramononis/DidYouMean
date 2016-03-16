883926a756913a1a7b4a679d7a0f34349889cb6c
Magnificus36 <magnificus@outlook.com>
2016-03-16 02:14:20 +0100

    org.hamcrest.MatcherAssert.assertThat instead of org.junit.Assert.assertThat

--- test/database/CSVControlTest.java
+++ test/database/CSVControlTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import java.util.HashMap;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Tests the CSVControl using the blackbox bethod.
--- test/database/IDBControlTest.java
+++ test/database/IDBControlTest.java
@@ -3,7 +3,7 @@ package database;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Tests the IDBControl.
