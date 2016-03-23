90aeb54eb8b51e7d29133672aea33434c813d855
Magnificus36 <magnificus@outlook.com>
2016-03-24 00:19:56 +0100

    Added getDYM_N to DidYouMean

--- src/api/didyoumean/DidYouMean.java
+++ src/api/didyoumean/DidYouMean.java
@@ -1,9 +1,12 @@
 package api.didyoumean;
 
-import api.didyoumean.bktree.BKTree;
-import api.didyoumean.levenshteinautomata.*;
 import api.database.IDBControl;
+import api.didyoumean.bktree.BKTree;
+import api.didyoumean.levenshteinautomata.LevenshteinAutomata;
+import api.didyoumean.levenshteinautomata.LevenshteinAutomataFactory;
 import api.tree.Root;
+
+import java.util.List;
 
 /**
  * The main class that holds all the information about the BK-trees and Levenshtein Automata.
@@ -58,6 +61,27 @@ public class DidYouMean {
             return getTree().getDYM(searchString);
         } else if (method == DYM.LEVENSHTEIN) {
             return LevenshteinAutomata.intersect(root, laf, searchString);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Gets the final did-you-mean suggestion, given a search string.
+     *
+     * @param searchString The user's search string.
+     * @param n            The maximum number of suggestions to return.
+     * @return A list with at most n words the user probably meant when searching for {@code word}.
+     * Sorted from most likely to least likely. May include {@code word}, if so this will be the first one.
+     * @throws IllegalArgumentException if {@code searchString} is {@code null}.
+     */
+    public List<String> getDYM_N(String searchString, int n) {
+        if (searchString == null) {
+            throw new IllegalArgumentException("Search string is null in DidYouMean.getDYMFromString.");
+        } else if (method == DYM.BKTREE) {
+            return getTree().getDYM_N(searchString, n);
+        } else if (method == DYM.LEVENSHTEIN) {
+            return LevenshteinAutomata.intersectN(root, laf, searchString, n);
         } else {
             return null;
         }
