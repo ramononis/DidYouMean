d7e4a526c07d2dbe8d07d4fe295a8de2fbf26668
Tim <tsonderen108@gmail.com>
2016-04-04 12:14:47 +0200

    complexity tests added

---
+++ test/performancetest/ComplexityTests.java
@@ -0,0 +1,147 @@
+package performancetest;
+
+import api.autocomplete.AutoCompleter;
+import api.database.IDBControl;
+import api.didyoumean.DYM;
+import api.didyoumean.DidYouMean;
+import api.didyoumean.bktree.BKTree;
+import api.didyoumean.levenshteinautomata.LevenshteinAutomataFactory;
+import api.tree.Root;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+
+/**
+ * Created by Tim on 31/03/2016.
+ */
+public class ComplexityTests {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Creating necessary objects..");
+        IDBControl dbc = new TestPurposeDBC();
+        DidYouMean dym0BK = new DidYouMean(dbc, DYM.BKTREE, 6);
+        DidYouMean dym0LS = new DidYouMean(dbc, DYM.LEVENSHTEIN, 6);
+        System.out.println("Done");
+
+        System.out.println("============================================");
+
+        System.out.println("Time complexity tests"); // ######## Time complexity tests
+
+        Long start;
+        Long end;
+        long delta;
+
+        System.out.println("\tInitializations"); // ######## Initializations
+
+        start = System.currentTimeMillis();
+        AutoCompleter ac = new AutoCompleter(dbc);
+        end = System.currentTimeMillis();
+        delta = end - start;
+        System.out.println("\t\tAC: " + delta + " milliseconds");
+
+        start = System.currentTimeMillis();
+        BKTree dymBK = new BKTree();
+        dymBK.buildTree(dbc.getData());
+        end = System.currentTimeMillis();
+        delta = end - start;
+        System.out.println("\t\tDYM BKTree: " + delta + " milliseconds");
+
+        start = System.currentTimeMillis();
+        LevenshteinAutomataFactory laf = new LevenshteinAutomataFactory(3);
+        end = System.currentTimeMillis();
+        delta = end - start;
+        System.out.println("\t\tLSA Factory (3): " + delta + " milliseconds");
+
+        start = System.currentTimeMillis();
+        Root dymLS = new Root();
+        for (Map.Entry e : dbc.getData().entrySet()) {
+            dymLS.addOrIncrementWord(String.valueOf(e.getKey()), (int) e.getValue());
+        }
+        end = System.currentTimeMillis();
+        delta = end - start;
+        System.out.println("\t\tDYM LSA: " + delta + " milliseconds");
+
+        System.out.println("\tSearches"); // ######## Searches
+
+        start = System.currentTimeMillis();
+        ac.getTopN(5, "");
+        end = System.currentTimeMillis();
+        delta = end - start;
+        System.out.println("\t\tAC (5 suggestions, empty string): " + delta + " milliseconds");
+
+        start = System.currentTimeMillis();
+        dym0BK.getDYM("4634773");
+        end = System.currentTimeMillis();
+        delta = end - start;
+        System.out.println("\t\tDYM BKTree (\"4634773\"): " + delta + " milliseconds");
+
+        start = System.currentTimeMillis();
+        dym0LS.getDYM("4634773");
+        end = System.currentTimeMillis();
+        delta = end - start;
+        System.out.println("\t\tDYM LSA (\"4634773\"): " + delta + " milliseconds");
+
+        ac = null;
+        dymBK = null;
+        dymLS = null;
+        laf = null;
+
+        System.out.println("============================================");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Space complexity tests"); // ######## Space complexity tests;
+
+        System.out.println("Open java mission control and keep track of the heap");
+        System.out.println("Please type how much KB the heap currently is and press enter.");
+        int startsize = Integer.parseInt(String.valueOf(in.readLine()));
+
+        int endsize;
+        int deltasize;
+
+        System.out.println("\tInitializations"); // ######## Initializations
+
+        System.out.println("\tPress enter to start the next test.");
+        in.readLine();
+        ac = new AutoCompleter(dbc);
+        System.out.println("\t\tPlease type how much KB the heap currently is and press enter.");
+        endsize = Integer.parseInt(String.valueOf(in.readLine()));
+        ac = null;
+        deltasize = endsize - startsize;
+        System.out.println("\t\tAC: " + deltasize + " KB");
+
+        System.out.println("\tPress enter to start the next test.");
+        in.readLine();
+        dymBK = new BKTree();
+        System.out.println("\t\tPlease type how much KB the heap currently is and press enter.");
+        endsize = Integer.parseInt(String.valueOf(in.readLine()));
+        dymBK = null;
+        deltasize = endsize - startsize;
+        System.out.println("\t\tBK Tree: " + deltasize + " KB");
+
+        System.out.println("\tPress enter to start the next test.");
+        in.readLine();
+        laf = new LevenshteinAutomataFactory(3);
+        System.out.println("\t\tPlease type how much KB the heap currently is and press enter.");
+        endsize = Integer.parseInt(String.valueOf(in.readLine()));
+        laf = null;
+        deltasize = endsize - startsize;
+        System.out.println("\t\tLevenshtein automata factory: " + deltasize + " KB");
+
+        System.out.println("\tPress enter to start the next test.");
+        in.readLine();
+        dymLS = new Root();
+        for (Map.Entry e : dbc.getData().entrySet()) {
+            dymLS.addOrIncrementWord(String.valueOf(e.getKey()), (int) e.getValue());
+        }
+        System.out.println("\t\tPlease type how much KB the heap currently is and press enter.");
+        endsize = Integer.parseInt(String.valueOf(in.readLine()));
+        dymLS = null;
+        deltasize = endsize - startsize;
+        System.out.println("\t\tDYM LSA: " + deltasize + " KB");
+
+        System.out.println("============================================");
+    }
+}
